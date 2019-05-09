09c3d49f3a53076d204f42db9c53d60414a8f89a
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-05-09 14:20:38 +0530

    entity's added

---
+++ src/main/java/com/hcl/dagobert/comm/app/dto/ProductCommisitionResponse.java
@@ -0,0 +1,9 @@
+package com.hcl.dagobert.comm.app.dto;
+
+import java.io.Serializable;
+
+public class ProductCommisitionResponse implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
