d38f00784055e615e25f6c2b7ab6d095c594cef1
admin1104 <49300434+admin1104@users.noreply.github.com>
2019-05-09 10:40:57 +0530

    initial setup

---
+++ src/main/java/com/hcl/dagobert/comm/app/CommmitionappApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.dagobert.comm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommmitionappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommmitionappApplication.class, args);
+	}
+
+}
